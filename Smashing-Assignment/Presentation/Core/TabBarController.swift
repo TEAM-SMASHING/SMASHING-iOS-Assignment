4d1bfe754eb71192abb84b22eb92f83fe2e67894
Rudy-009 <rofxnaos@gmail.com>
2025-12-24 17:22:19 +0900

    [feat] TabBar 설정

---
+++ Smashing-Assignment/Presentation/Core/TabBarController.swift
@@ -0,0 +1,120 @@
+//
+//  TabBarController.swift
+//  Smashing-Assignment
+//
+//  Created by 이승준 on 12/24/25.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    //MARK: - Properties
+    
+    private let defaultTab: Tab = .jinjae
+    
+    enum Tab: Int, CaseIterable {
+        case jinjae = 0
+        case junbeom
+        case seungjun
+        
+        var imageName: UIImage {
+            switch self {
+            case .jinjae: return UIImage(systemName: "figure.basketball")!
+            case .junbeom: return UIImage(systemName: "figure.table.tennis")!
+            case .seungjun: return UIImage(systemName: "figure.badminton")!
+            }
+        }
+        
+        var selectedImageName: UIImage {
+            switch self {
+            case .jinjae: return UIImage(systemName: "figure.basketball.circle.fill")!
+            case .junbeom: return UIImage(systemName: "figure.table.tennis.circle.fill")!
+            case .seungjun: return UIImage(systemName: "figure.badminton.circle.fill")!
+            }
+        }
+        
+        var viewController: UIViewController {
+            switch self {
+            case .jinjae:
+              return UIViewController()
+            case .junbeom:
+                return UIViewController()
+            case .seungjun:
+                return UIViewController()
+            }
+        }
+    }
+    
+    //MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        
+        setViewControllers()
+        setTabBarAppearance()
+        selectedIndex = defaultTab.rawValue
+    }
+    
+    //MARK: - Private Methods
+    
+    private func setViewControllers() {
+        let topMargin: CGFloat = 7.0
+            
+        self.viewControllers = Tab.allCases.map { tab in
+            let rootVC = tab.viewController
+            let nav = UINavigationController(rootViewController: rootVC)
+            nav.isNavigationBarHidden = true
+            
+            let icon = resizeImage(image: tab.imageName).withRenderingMode(.alwaysOriginal)
+            let selectedIcon = resizeImage(image: tab.selectedImageName).withRenderingMode(.alwaysOriginal)
+            
+            nav.tabBarItem = UITabBarItem(title: nil,
+                                          image: icon,
+                                          selectedImage: selectedIcon)
+            nav.tabBarItem.tag = tab.rawValue
+            nav.tabBarItem.imageInsets = UIEdgeInsets(top: topMargin, left: 0,
+                                                      bottom: -topMargin, right: 0)
+            
+            return nav
+        }
+    }
+    
+    private func setTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .white
+        appearance.shadowColor = .darkGray
+        
+        let itemAppearance = UITabBarItemAppearance()
+        
+        appearance.stackedLayoutAppearance = itemAppearance
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+    }
+    
+    
+    private func resizeImage(image: UIImage) -> UIImage {
+        let targetSize = CGSize(width: 48, height: 48)
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let ratio = min(widthRatio, heightRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        image.draw(in: CGRect(origin: .zero, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage ?? image
+    }
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+}
